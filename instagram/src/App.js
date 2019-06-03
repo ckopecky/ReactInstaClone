fabb0b5dfd03becd5b1407a2fece04c3d92aa22b
ckopecky <kopecky12112@gmail.com>
2019-06-03 00:24:53 -0700

    main scaffold with search, postcontainer and comments

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import dummydata from "./dummy-data";
+import CommentSection from './components/CommentSection/CommentSection';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dummydata: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({dummydata})
+  }
+
+  addComment = (comment, user) => {
+
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <SearchBar />
+{this.state.dummydata.map(post => {
+          return (
+            <div key={post.username + post.likes} className="instagram-container">
+              <div className="post-container">
+                <PostContainer 
+                  username={post.username}
+                  image={post.imageUrl}
+                  profile={post.thumbnailUrl}
+                  numLikes={post.likes}
+                  timestamp={post.timestamp}
+                />
+                <CommentSection   comments={post.comments}
+                />
+              </div>
+            </div>
+          )
+        })}
+        
+      </div>
+    );
+  }
 }
 
 export default App;
