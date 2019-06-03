be02cb1de27741975e29d8945aa948ef55b863a2
ckopecky <kopecky12112@gmail.com>
2019-06-03 00:26:39 -0700

    Scaffolding of PostContainer

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import dummydata from '../../dummy-data';
 import PropTypes from 'prop-types';
+import PostHeader from './PostHeader';
+import Img from './Img';
+import Footer from './Footer';
 
-const PostContainer = props => {
+const PostContainer = props => { 
     return (
         <div>
-            {/* header */}
-            {/* image */}
-            {/* footer */}
+            <PostHeader 
+                profilePic={props.profile}
+                username={props.username} 
+                timestamp={props.timestamp}
+            />
+            <Img
+                image={props.image} 
+            />
+            <Footer 
+                likes={props.numLikes}
+            />
         </div>
     );
-};
+}
+
 
 PostContainer.propTypes = {
-    
+
 };
 
 export default PostContainer;
