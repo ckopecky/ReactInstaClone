7bb1cda846e2ef4c6b5cb1689e15967df9a1c5b8
ckopecky <kopecky12112@gmail.com>
2019-06-03 00:27:35 -0700

    Scaffolding of Search Bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import InstaLogo from './InstaLogo';
+import InstagramIcon from "../../assets/instalogo.png";
+import InstagramLogo from "../../assets/instagram_logo.jpg";
+import SearchInput from './SearchInput';
+import IconContainer from './IconContainer';
 
 const SearchBar = () => {
     return (
-        <div>
-            {/* logo */}
-            {/* search input */}
-            {/* icons */}
+        <div className="search-container">
+            <InstaLogo 
+                icon={InstagramIcon}
+                logo={InstagramLogo}
+            />
+            <SearchInput />
+            <IconContainer />
         </div>
     );
 };
