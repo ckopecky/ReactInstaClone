a39101bdf3962c04bebaac5a82133b641a2aad11
ckopecky <kopecky12112@gmail.com>
2019-06-03 00:26:04 -0700

    Comment Section scaffolded out

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import CommentForm from './CommentForm';
 
-const CommentSection = props => {
-    return (
-        <div>
-            {/* comments */}
-            {/* form for new comment */}
-        </div>
-    );
+class CommentSection extends React.Component {
+
+    render(){
+    
+        return (
+            <div>
+                {this.props.comments.map(comment => {
+                    return (
+                        <div className="comment-box">
+                            <p><strong>{comment.username}</strong></p>
+                            <p>{comment.text}</p>
+                            <p><FontAwesomeIcon icon={faHeart}/></p>
+                        </div>
+                    )
+                })}
+                <CommentForm />
+            </div>
+        );
+    }
 };
 
 CommentSection.propTypes = {
